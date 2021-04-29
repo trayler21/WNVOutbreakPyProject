7272babd2ea43fba37faf8794c899e0e6c127d57
Jodi Jade Trayler <jtrayler@student.cccs.edu>
2021-04-29 17:12:38 -0600

    Lab 3 Practice (Check export_map, please!)

--- lab3.py
+++ lab3.py
@@ -1,34 +1,37 @@
-import yaml
-import arcpy
 import logging
+import arcpy
+import yaml
 from etl.GSheetsEtl import GSheetsEtl
 
 
+# Define the setup, complete with logging messaging capabilities.
 def setup():
     with open('config/wnvoutbreak.yaml') as f:
         config_dict = yaml.load(f, Loader=yaml.FullLoader)
-        return config_dict
 
+    logging.basicConfig(filename=f"{config_dict.get('proj_dir')}wnv.log",
+                        filemode='w',
+                        level=logging.DEBUG)
 
-logging.basicConfig(filename=f"{config_dict.get('proj_dir')}wnv.log",
-                    filemode="w",
-                    level=logging.DEBUG)
+    logging.debug('This is a debug statement!')
+    logging.info('This will get logged to a file!')
+    logging.warning('This is a Warning!')
+    logging.error('This is an Error!')
 
-logging.debug('This is a debug statement')
-logging.info('This will get logged to a file')
-logging.warning('This is a Warning')
-logging.error('This is an error!')
+    return config_dict
 
 
 # Define the ETL.
 def etl():
-    print("Start etl process...")
+    logging.debug("Starting ETL Method...")
     etl_instance = GSheetsEtl(config_dict)
     etl_instance.process()
+    logging.debug("Ending ETL Method!")
 
 
 # Define the buffer function.
 def buffer(layer, dist):
+    logging.debug("Starting Buffer Method...")
     units = " feet"
     dist = dist + units
     output_layer = layer + "_buf"
@@ -37,25 +40,24 @@ def buffer(layer, dist):
     for fc in fc_list:
         if output_layer == fc:
             arcpy.Delete_management(fc)
-            print(f"Deleted {fc}")
-
     arcpy.Buffer_analysis(layer, output_layer, dist, "FULL", "ROUND", "ALL")
-    print("Buffer created " + output_layer)
+    logging.debug("Ending Buffer Method!")
     return output_layer
 
 
 # Define the intersect function.
 def intersect(inter_list):
-    print(inter_list)
+    logging.debug("Starting Intersect Method...")
     output_layer = input("Please name the intersect layer to be created: ")
     arcpy.Intersect_analysis(inter_list, output_layer)
     print("Intersect created.")
+    logging.debug("Ending Intersect Method!")
     return output_layer
 
 
 # Define the main function.
 def main():
-    print("Starting West Nile Virus Simulation")
+    logging.debug("Starting West Nile Virus Simulation...")
     arcpy.env.overwriteOutput = True
     arcpy.env.workspace = f"{config_dict.get('proj_dir')}WestNileOutbreak.gdb"
     aprx = arcpy.mp.ArcGISProject(r"C:\Users\Owner\Documents\ArcGIS\Projects\WestNileOutbreak\WestNileOutbreak.aprx")
@@ -64,18 +66,17 @@ def main():
         for lyr in map.listLayers():
             print("- " + lyr.name)
 
-    # Create container for layers.
+# Create container for layers.
     layer_list = ["Mosquito_Larval_Sites", "Wetlands_Regulatory", "OSMP_Properties", "Lakes_and_Reservoirs",
                   "avoid_points"]
-    print("These five map layers will be buffered to the extent you choose: ")
-    print(layer_list)
 
-    # Define workspace.
+# Define workspace.
     resultsgeodatabase = r"C:\Users\Owner\Documents\ArcGIS\Projects\WestNileOutbreak\WestNileOutbreak.gdb\\"
     arcpy.env.workspace = resultsgeodatabase
 
     featureclass = arcpy.ListFeatureClasses()
-    print(arcpy.ListFeatureClasses())
+
+    logging.debug("Ending West Nile Virus Simulation!")
 
     for layer in layer_list:
         print(layer)
@@ -85,10 +86,10 @@ def main():
         bufferlayer = buffer(layer, dist)
 
     featureclass = arcpy.ListFeatureClasses()
-    print(arcpy.ListFeatureClasses())
 
     inter_list = ["Mosquito_Larval_Sites_buf", "Wetlands_Regulatory_buf", "OSMP_Properties_buf",
                   "Lakes_and_Reservoirs_buf"]
+
     output_intersectlayer = intersect(inter_list)
 
     target_features = "Boulder_addresses"
@@ -105,6 +106,18 @@ def main():
 
     arcpy.Erase_analysis(target_features, erase_features, out_feature_class)
     print("Erase layer created.")
+
+
+# Define the export function.
+def export_map():
+    aprx = arcpy.mp.ArcGISProject(f"{config_dict.get('proj_dir')}WestNileOutbreak.aprx")
+    lyt = aprx.listLayouts()[0]
+    subtitle = input("Please provide a subtitle for the West Nile Virus Outbreak map: ")
+    for el in lyt.listElements():
+        print(el.name)
+        if "Title" in el.name:
+            el.text = el.text + subtitle
+
     print("Have a great day!")
 
 
